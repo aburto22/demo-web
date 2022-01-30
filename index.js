1b1e6c7e44d071a41c4f97dc96894e0773d28cb1
aburto22 <aburto22@gmail.com>
2022-01-30 11:16:53 +0100

    basic server added

--- index.js
+++ index.js
@@ -1,48 +1,11 @@
- document.querySelector('#app-roman button').addEventListener('click', () => {
-  const romanInput = document.querySelector('#app-roman input').value;
-  const romanOutput = romanToNum(romanInput);
-  document.querySelector('#app-roman .output').innerHTML = `<p>${romanOutput}</p>`;
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('hello world!');
 });
 
-document.querySelector('#app-prime-factors button').addEventListener('click', () => {
-  const primeInput = document.querySelector('#app-prime-factors input').value;
-  const primeOutput = findPrimeFactors(Number(primeInput));
-  document.querySelector('#app-prime-factors .output').innerHTML = `<p>${primeOutput}</p>`;
-});
+const PORT = process.env.PORT || 5500;
 
-function hideAllDivs() {
-  document.querySelector('.main-image').style.display = 'none';
-  document.querySelectorAll('.app').forEach(element => element.style.display = 'none');
-}
-
-function resetButtonStyle() {
-  document.querySelectorAll('.section').forEach(element => element.style.backgroundColor = 'transparent');
-}
-
-document.querySelector('#roman').addEventListener('click', () => {
-  hideAllDivs();
-  resetButtonStyle();
-  document.querySelector('#roman').style.backgroundColor = '#f0ad4e';
-  document.querySelector('#app-roman').style.display = 'block'; 
-});
-
-document.querySelector('#prime').addEventListener('click', () => {
-  hideAllDivs()
-  resetButtonStyle();
-  document.querySelector('#prime').style.backgroundColor = '#f0ad4e';
-  document.querySelector('#app-prime-factors').style.display = 'block';
-});
-
-document.querySelector('#funny').addEventListener('click', () => {
-  hideAllDivs()
-  resetButtonStyle();
-  document.querySelector('#funny').style.backgroundColor = '#f0ad4e';
-  document.querySelector('#funny-section').style.display = 'block';
-});
-
-document.querySelector('#hard').addEventListener('click', () => {
-  hideAllDivs()
-  resetButtonStyle();
-  document.querySelector('#hard').style.backgroundColor = '#f0ad4e';
-  document.querySelector('#hard-section').style.display = 'block';
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
